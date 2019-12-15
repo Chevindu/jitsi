c963e33e306b1b2e133cddbf1a5d5e09ed01324c
Isira K <isira2702@gmail.com>
2019-12-15 22:04:01 +0530

    Genesis

---
+++ index.js
@@ -0,0 +1,60 @@
+var express = require('express');
+var app = express();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+
+app.use(express.static('static'));
+
+io.on('connection', function (socket) {
+	console.log('a client connected');
+
+	socket.on('up', () => {
+		console.log('Input command: UP');
+		socket.broadcast.emit('up');
+	});
+	socket.on('down', () => {
+		console.log('Input command: DOWN');
+		socket.broadcast.emit('down');
+	});
+	socket.on('left', () => {
+		console.log('Input command: LEFT');
+		socket.broadcast.emit('left');
+	});
+	socket.on('right', () => {
+		console.log('Input command: RIGHT');
+		socket.broadcast.emit('right');
+	});
+	socket.on('upright', () => {
+		console.log('Input command: UP RIGHT');
+		socket.broadcast.emit('upright');
+	});
+	socket.on('downright', () => {
+		console.log('Input command: DOWN RIGHT');
+		socket.broadcast.emit('downright');
+	});
+	socket.on('upleft', () => {
+		console.log('Input command: UP LEFT');
+		socket.broadcast.emit('upleft');
+	});
+	socket.on('downleft', () => {
+		console.log('Input command: DOWN LEFT');
+		socket.broadcast.emit('downleft');
+	});
+	socket.on('brake', () => {
+		console.log('Input command: BRAKE');
+		socket.broadcast.emit('brake');
+	});
+	
+	socket.on('disconnect', () => {
+		console.log('user disconnected');
+	});
+
+	socket.on('error', (error) => {
+		console.log('Error: ', error);
+	});
+});
+
+http.listen(3000, () => {
+	console.log('Control interface is live on http://localhost:3000');
+});
---
+++ static/app.js
@@ -0,0 +1,159 @@
+let socket = io({
+    autoConnect: false
+});
+
+// Jitsi meet config
+const domain = 'meet.jit.si';
+const options = {
+    roomName: 'fypwebui',
+    width: 500,
+    height: 500,
+    parentNode: document.querySelector('#meet')
+};
+
+// Toggle Jitsi element
+let api = null;
+function toggleCall() {
+    if (socket.connected && api === null) {
+        document.querySelector('.meet').innerHTML = ''
+        api = new JitsiMeetExternalAPI(domain, options);
+    } else if (api !== null) {
+        api.dispose();
+        api = null;
+        document.querySelector('.meet').innerHTML = '<img src="bg.svg" />'
+    }
+}
+
+// Websocket toggle
+$(function () {
+    $('#chkToggle').bootstrapToggle({
+        on: 'Connected',
+        off: 'Disconnected'
+    })
+    $('#chkToggle').bootstrapToggle('off');
+});
+function toggleWS() {
+    if (socket.disconnected) {
+        socket.open();
+        $('#chkToggle').bootstrapToggle('on');
+        $('#wsToggleBtn').html('Disconnect')
+    } else {
+        socket.close();
+        $('#chkToggle').bootstrapToggle('off');
+        $('#wsToggleBtn').html('Connect')
+    }
+}
+socket.on('connect', () => {
+    console.log(`WebSocket connection status: ${socket.connected}`);
+    // console.log('Connected to the Robot');
+    toggleCall();
+
+});
+socket.on('disconnect', function (reason) {
+    console.log(`WebSocket disconnected. reason: ${reason}`)
+    // console.log('Disconnected from the Robot');
+    toggleCall();
+});
+
+// Command funcs
+function goUp() {
+    if (!socket.connected) {
+        console.log("No WebSocket connection.");
+    } else {
+        socket.emit('up');
+        console.log("Command: Up");
+    }
+}
+function goDown() {
+    if (!socket.connected) {
+        console.log("No WebSocket connection.");
+    } else {
+        socket.emit('down');
+        console.log("Command: Down");
+    }
+}
+function goLeft() {
+    if (!socket.connected) {
+        console.log("No WebSocket connection.");
+    } else {
+        socket.emit('left');
+        console.log("Command: Left");
+    }
+}
+function goRight() {
+    if (!socket.connected) {
+        console.log("No WebSocket connection.");
+    } else {
+        socket.emit('right');
+        console.log("Command: Right");
+    }
+}
+function goUpRight() {
+    if (!socket.connected) {
+        console.log("No WebSocket connection.");
+    } else {
+        socket.emit('upright');
+        console.log("Command: Up Right");
+    }
+}
+function goDownRight() {
+    if (!socket.connected) {
+        console.log("No WebSocket connection.");
+    } else {
+        socket.emit('downright');
+        console.log("Command: Down Right");
+    }
+}
+function goUpLeft() {
+    if (!socket.connected) {
+        console.log("No WebSocket connection.");
+    } else {
+        socket.emit('upleft');
+        console.log("Command: Up Left");
+    }
+}
+function goDownLeft() {
+    if (!socket.connected) {
+        console.log("No WebSocket connection.");
+    } else {
+        socket.emit('downleft');
+        console.log("Command: Down Left");
+    }
+}
+function brake() {
+    if (!socket.connected) {
+        console.log("No WebSocket connection.");
+    } else {
+        socket.emit('brake');
+        console.log("Command: Brake");
+    }
+}
+
+
+//Keyboard commands interfacing
+let pressed = {}; // could also use an array
+onkeydown = onkeyup = function (e) {
+    e = e || event; // to deal with IE
+    pressed[e.keyCode] = e.type == 'keydown';
+    /* insert conditional here */
+    if (pressed[38] && pressed[39]) {
+        goUpRight();
+    } else if (pressed[38] && pressed[37]) {
+        goUpLeft();
+    } else if (pressed[40] && pressed[39]) {
+        goDownRight();
+    } else if (pressed[40] && pressed[37]) {
+        goDownLeft();
+    } else if (pressed[38]) {
+        goUp();
+    } else if (pressed[40]) {
+        goDown();
+    } else if (pressed[37]) {
+        goLeft();
+    } else if (pressed[39]) {
+        goRight();
+    } else if (pressed[32]) {
+        brake();
+    }
+}
+
